ebb57bc11155ffe7968b62ba8465214556c963e7
shrey-958 <shreydedhia@yahoo.in>
2019-10-12 21:01:28 +0530

    Updates

---
+++ precache-manifest.cb706be8f0f284c23dd2826e425c70ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4073a19534bd63f10dcb1a4b8b073416",
+    "url": "/cinematic-app/index.html"
+  },
+  {
+    "revision": "a047e5d4e55ec71dfcb8",
+    "url": "/cinematic-app/static/css/main.8f7078db.chunk.css"
+  },
+  {
+    "revision": "5a5cdb415873b17cf1dc",
+    "url": "/cinematic-app/static/js/2.792b69b8.chunk.js"
+  },
+  {
+    "revision": "a047e5d4e55ec71dfcb8",
+    "url": "/cinematic-app/static/js/main.2d6aa241.chunk.js"
+  },
+  {
+    "revision": "3928351ff0787aff5174",
+    "url": "/cinematic-app/static/js/runtime-main.53a754ed.js"
+  }
+]);
